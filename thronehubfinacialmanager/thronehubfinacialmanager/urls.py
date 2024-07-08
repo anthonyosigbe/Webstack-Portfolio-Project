143f35269585327efe22b75a5f1147566b797926
Anthony Azubike Osigbe <thronesofglory@gmail.com>
2024-07-08 07:22:59 +0100

    file update

--- thronehubfinacialmanager/thronehubfinacialmanager/urls.py
+++ thronehubfinacialmanager/thronehubfinacialmanager/urls.py
@@ -7,9 +7,18 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Include URLs from 'expenses' app
     path('', include('expenses.urls')),
+    
+    # Include URLs from 'authentication' app
     path('authentication/', include('authentication.urls')),
+    
+    # Include URLs from 'userpreferences' app
     path('preferences/', include('userpreferences.urls')),
+    
+    # Include URLs from 'userincome' app
     path('income/', include('userincome.urls')),
+    
+    # Admin site URL
     path('admin/', admin.site.urls),
 ]
